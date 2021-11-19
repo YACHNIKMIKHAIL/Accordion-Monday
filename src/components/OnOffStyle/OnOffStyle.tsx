2d108c07e5649a695ce4ada9b584b9f4d46f8a27
YACHNIKMIKHAIL <92398592+YACHNIKMIKHAIL@users.noreply.github.com>
2021-11-19 18:32:23 +0300

    Add files via upload

---
+++ src/components/OnOffStyle/OnOffStyle.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+
+type OnOffStyleType = {
+    // on: boolean
+}
+
+export const OnOff = (props: OnOffStyleType) => {
+    console.log('OnOffStyle rendered!')
+
+    let [on, setOn] = useState(false);
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px black solid',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: on ? 'green' : 'white'
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px black solid',
+        display: 'inline-block',
+        padding: '2px',
+        marginLeft: '2px',
+        backgroundColor: on ? 'white' : 'red'
+    }
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px black solid',
+        display: 'inline-block',
+        marginLeft: '2px',
+        backgroundColor: on ? 'green' : 'red'
+    }
+
+    return (
+        <div>
+            <div style={onStyle} onClick={() => setOn(true)}>On</div>
+            <div style={offStyle} onClick={() => setOn(false)}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
